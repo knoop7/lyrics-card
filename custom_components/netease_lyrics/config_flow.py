38c8e60e362f05df0a87fb6a15edc9b2a9d6cbe0
KNOOP <knuop@icloud.com>
2025-04-27 11:47:53 +0800

    Update config_flow.py

--- custom_components/netease_lyrics/config_flow.py
+++ custom_components/netease_lyrics/config_flow.py
@@ -1,18 +1,25 @@
 from __future__ import annotations
 
 import voluptuous as vol
+import sys
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN, CONF_SOURCE, DEFAULT_SOURCE, SOURCES
 
+try:
+    from homeassistant.helpers.typing import ConfigType
+    NEW_HOMEASSISTANT = True
+except ImportError:
+    NEW_HOMEASSISTANT = False
+
 class NeteaseLyricsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
     async def async_step_user(
-        self, user_input: dict[str, any] | None = None
+        self, user_input=None
     ) -> FlowResult:
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
@@ -37,31 +44,35 @@ class NeteaseLyricsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(
         config_entry: config_entries.ConfigEntry,
-    ) -> NeteaseLyricsOptionsFlow:
+    ):
         return NeteaseLyricsOptionsFlow(config_entry)
 
 
 class NeteaseLyricsOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self._config_entry = config_entry  
-        super().__init__()  
+        if NEW_HOMEASSISTANT:
+            self._config_entry = config_entry  
+            super().__init__()
+        else:
+            self.config_entry = config_entry
 
     async def async_step_init(
-        self, user_input: dict[str, any] | None = None
-    ) -> FlowResult:
+        self, user_input=None
+    ):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
+
+        config_entry = self._config_entry if NEW_HOMEASSISTANT else self.config_entry
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Required(
                     CONF_SOURCE,
-                    default=self._config_entry.options.get(  
+                    default=config_entry.options.get(  
                         CONF_SOURCE, 
-                        self._config_entry.data.get(CONF_SOURCE, DEFAULT_SOURCE)
+                        config_entry.data.get(CONF_SOURCE, DEFAULT_SOURCE)
                     ),
                 ): vol.In(SOURCES),
             }),
